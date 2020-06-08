13467d814d015a9e57e4abdc2935d79c56de9386
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-06-08 17:20:31 +0200

    models

---
+++ db/migrate/20200608151753_create_choices.rb
@@ -0,0 +1,11 @@
+class CreateChoices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :choices do |t|
+      t.text :content
+      t.boolean :correctness
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
