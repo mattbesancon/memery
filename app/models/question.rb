35c69ef2aeaeb272a6964792d43e066da9b2dba2
Matthieu Besancon <matthieu.besancon@gmail.com>
2020-06-13 16:19:16 +0200

    questions model methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,25 @@
 class Question < ApplicationRecord
-    has_many :choices
+  has_many :choices
+
+  def answer
+    uncorrect
+    choices.select {|c| c.correct}[0]
+  end	
+ 
+  def uncorrect
+    choices.each {|c| c.correct = false}
+  end
+ 
+  def answer= choice
+    if !answer.nil?
+       answer.correct = false
+    end
+    
+    if choices.include? choice
+       choice.correct = true
+    else
+       choices << choice
+       choice.correct = true
+    end
+  end
 end
